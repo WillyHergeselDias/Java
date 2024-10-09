eb38253d104376049432ea6138b885caa86ebc07
Boing <148807368+WillyHergeselDias@users.noreply.github.com>
2024-10-09 09:47:25 -0300

    Update

---
+++ Classes.java
@@ -0,0 +1,12 @@
+public class main{
+ public static void main(String[] args){
+  HelloWorld helloworld = new HelloWorld();
+  helloworld.helloworld();
+ }
+}
+
+class HelloWorld{
+ public void helloworld(){
+  System.out.println("Hello World!");
+ }
+}
