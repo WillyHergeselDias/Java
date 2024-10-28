f400276f3587d8ff54d446c51d652e30e3dfd947
Boing <148807368+WillyHergeselDias@users.noreply.github.com>
2024-10-27 22:34:41 -0300

    Update Multiplication_Table.java

--- Multiplication_Table.java
+++ Multiplication_Table.java
@@ -4,9 +4,9 @@ public class Program
 {
     public static void main(String[] args){
      Scanner input = new Scanner(System.in);
-     int num = input.nextInt();
+     int n = input.nextInt();
      for(int i = 1; i <= 10; i++){
-         System.out.println(String.valueOf(num) + " x "  + String.valueOf(i) + " = " + num*i);
+         System.out.println(n + " x "  + i + " = " + num*i);
      }
     }
 }
